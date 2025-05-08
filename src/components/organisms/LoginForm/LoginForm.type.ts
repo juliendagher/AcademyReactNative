133887f7495650412d2216cf2dc712abb94cff63
Julien Dagher <juliendagher@outlook.com>
2025-05-08 22:31:49 +0300

    fixed small issue in login and added signup files

--- src/components/organisms/LoginForm/LoginForm.type.ts
+++ src/components/organisms/LoginForm/LoginForm.type.ts
@@ -4,7 +4,7 @@ import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
 const schema = z.object({
   email: z.string().email('Please enter a valid email'),
-  password: z.string().min(8, 'Password must be at least 8 characters'),
+  password: z.string().min(1, 'Password cannot be empty'),
 });
 
 type LoginFormData = z.infer<typeof schema>;
