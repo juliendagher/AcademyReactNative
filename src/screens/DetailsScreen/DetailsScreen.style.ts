a9202c2e44349e22ec37376f8d5ad7fb193b61ab
Julien Dagher <juliendagher@outlook.com>
2025-05-10 18:10:21 +0300

    basic details screen, minimal styling

---
+++ src/screens/DetailsScreen/DetailsScreen.style.ts
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    height: 350,
+    width: '100%',
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+});
+
+export {styles};
--- src/screens/DetailsScreen/DetailsScreen.tsx
+++ src/screens/DetailsScreen/DetailsScreen.tsx
@@ -1,14 +1,31 @@
-import {View, Text} from 'react-native';
+import {View, Text, Image, ScrollView} from 'react-native';
 import React from 'react';
 import {useRoute} from '@react-navigation/native';
 import {DetailsRouteProp} from './DetailsScreen.type';
+import {data} from '../../assets/Products.json';
+import {Error} from '../../components/atoms/Error';
+import {Label} from '../../components/atoms/Label';
+import {PressableWrapper} from '../../components/atoms/PressableWrapper';
+import { styles } from './DetailsScreen.style';
 
 const DetailsScreen = () => {
   const {params} = useRoute<DetailsRouteProp>();
+  const {id} = params;
 
-  return (
+  const product = data.find(item => item._id === id);
+
+  return product ? (
+    <ScrollView>
+      <Image style={styles.image} source={{uri: product.images[0].url}} />
+      <Label title={product.title} />
+      <Text>Price: {product.price}$</Text>
+      <Text>{product.description}</Text>
+      <PressableWrapper label="Add to cart" />
+      <PressableWrapper label="Share" />
+    </ScrollView>
+  ) : (
     <View>
-      <Text>{params.id}</Text>
+      <Error errorMessage="Couldn't find requested product" />
     </View>
   );
 };
