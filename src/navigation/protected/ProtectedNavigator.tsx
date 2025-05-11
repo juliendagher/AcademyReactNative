b2e5e8054a9d25e494da78879a14a1a78ec5b723
Julien Dagher <juliendagher@outlook.com>
2025-05-11 19:59:18 +0300

    bottom tabs styling

--- src/navigation/protected/ProtectedNavigator.tsx
+++ src/navigation/protected/ProtectedNavigator.tsx
@@ -7,6 +7,8 @@ import {
 } from './ProtectedNavigator.type';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {SettingsScreen} from '../../screens/SettingsScreen';
+import {useTheme} from '../../hooks/theme';
+import {verticalScale} from '../../utils/scaling/scale';
 
 const Stack = createNativeStackNavigator<ProtectedStackParamList>();
 const Tab = createBottomTabNavigator<ProtectedTabParamList>();
@@ -21,10 +23,36 @@ const HomeStack = () => {
 };
 
 const ProtectedNavigator = () => {
+  const {colors} = useTheme();
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
-      <Tab.Screen name="Home" component={HomeStack} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarIcon: () => null,
+        tabBarIconStyle: {height: 0, width: 0},
+        tabBarLabelStyle: {
+          marginTop: verticalScale(10),
+        },
+        tabBarStyle: {
+          backgroundColor: colors.background,
+        },
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeStack}
+        options={{
+          tabBarLabel: 'Shop',
+          tabBarActiveTintColor: colors.tertiary,
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarLabel: 'Settings',
+          tabBarActiveTintColor: colors.tertiary,
+        }}
+      />
     </Tab.Navigator>
   );
 };
